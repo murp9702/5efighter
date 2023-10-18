95de26098b1d2a69c24beb9498e9f2d09f94d4aa
Brennen <brennenamurphy@gmail.com>
2023-10-17 22:02:11 -0600

    making progress. currently broken build, will fix later

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import requests
 import random
 
+#TODO
+# need to implement check for "multiattack" to reduce attack_bonus key errors
+# need to rebuild attack_damage, the dice are hidden inside another layer of the object
 
 class Fighter:
     def __init__(self, jsonInfo):
         self.name = jsonInfo['name']
         self.hit_points = jsonInfo['hit_points']
         self.armor_class = jsonInfo['armor_class'][0]['value']
-        self.actions = jsonInfo['actions']
+        self.attack_bonus = jsonInfo['actions'][0]['attack_bonus']
         self.initiative = roll_die(20) + proficiency_math(jsonInfo['dexterity'])
+        self.attack_damage = [jsonInfo['actions'][0]['damage_dice'].split('d')[0],
+                              jsonInfo['actions'][0]['damage_dice'].split('d')[1].split('+')[0],
+                              jsonInfo['actions'][0]['damage_dice'].split('+')[1]
+        ]
+
         
 
 
-def roll_die(dN):
-    return random.randint(1, dN)
+def roll_die(die, numberOfDie=1):
+    return random.randint(1, die)
 
 def proficiency_math(rawScore):
     return int((rawScore - 10) / 2)
@@ -33,10 +41,30 @@ def build_fighters(monsterList):
             i += 1
             yield Fighter(monster)
 
+
+def set_initiative(fighter1, fighter2):
+    if fighter2.initiative > fighter1.initiative:
+        fighter2, fighter1 = fighter1, fighter2
+
+
+def conscious_check(fighter):
+    if fighter.hit_points <= 0:
+        return False
+
+
+def attack_vs_armor_class(attack, armorClass):
+    attack += roll_die(20)
+    if attack >= armorClass:
+        return True
+    else:
+        return False
+
 def fight(fighter1, fighter2):
-    if int(fighter1.hit_points) and int(fighter2.hit_points) > 0:
-        fighter1.hit_points -= 5
-        print(fighter1.hit_points)
+
+    if attack_vs_armor_class(fighter1.attack_bonus, fighter2.armor_class):
+        print("HIT")
+        roll_die(fighter1.attack_damage)
+
 
 
 
@@ -46,7 +74,8 @@ if __name__ == '__main__':
     # grab the list of all monsters from the api. this will be used to generate random encounters
     monsters = call_api('https://www.dnd5eapi.co/api/monsters')
     fighter1, fighter2 = build_fighters(monsters)
-    print(fighter2.initiative)
-    # print(fighter1.actions)
-    # fight(fighter1, fighter2)
-
+    set_initiative(fighter1, fighter2)
+    fight(fighter1, fighter2)
+    print(fighter1.attack_bonus)
+    print("||||||||||||||||||||||||||||")
+    print(fighter2.attack_bonus)
